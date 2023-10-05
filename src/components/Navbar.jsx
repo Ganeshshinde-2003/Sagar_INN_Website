f182efeb8f089a2e10f3f0329b01af609156fb92
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-10-05 15:48:41 +0530

    created the about and menu page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import "./style.css";
 import logo from "../assets/logo-hotle.png";
-import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const currentPath = useLocation();
+
   return (
     <div className="navbar-container">
-      <div className="navbar-home">
-        <Link to="/">
-          Home <IoIosArrowDown />
+      <div
+        className={`navbar-home ${
+          currentPath.pathname === "/menu" ? "active" : ""
+        }`}
+      >
+        <Link to="/menu">
+          Menu{" "}
+          {currentPath.pathname === "/menu" ? (
+            <IoIosArrowUp />
+          ) : (
+            <IoIosArrowDown />
+          )}
         </Link>
       </div>
       <div className="navbar-logo">
